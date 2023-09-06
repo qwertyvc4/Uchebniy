30cadb9e55f1f794729c91b3c728136e615770d4
qwertyvc4 <135412875+qwertyvc4@users.noreply.github.com>
2023-09-06 09:21:08 +0300

    Add files via upload

---
+++ Прак 4-2.1/Прак 4-2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Прак_4_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int s = 0;
+            int k = 0;
+            do
+            {
+                Console.WriteLine(",,,");
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a > 0)
+                {
+                    s = s + a;
+                }
+            }
+            while (a != 0);
+
+            if (k > 0)
+            {
+                Console.WriteLine("dfg = " + s);
+            }
+            else
+                Console.WriteLine("---");
+        }
+    }
+}
---
+++ прак 4-2/прак 4-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.NetworkInformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace прак_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вводим a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Вводим b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            for (int i = a; i <= b; i++)
+                s += i;
+            Console.WriteLine("Сумма = " + s);
+            Console.ReadKey();
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+    }
+}
