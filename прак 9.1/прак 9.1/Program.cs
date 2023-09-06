9ee59c355515cb4c9d41749f2a1274d00b67d7a7
qwertyvc4 <135412875+qwertyvc4@users.noreply.github.com>
2023-09-06 09:29:36 +0300

    Add files via upload

---
+++ прак 9.1/прак 9.1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace прак_9._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string t = Console.ReadLine();
+            int s = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                Console.WriteLine(t[i]);
+                int a=(int)Char.GetNumericValue(t[i]);
+                s = s + a;
+            }
+            Console.WriteLine("Сумма = " + s);
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+    }
+}
