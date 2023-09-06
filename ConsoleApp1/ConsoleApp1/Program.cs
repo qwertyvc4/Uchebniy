cf8fdd96819f2d6172fe5843957318bb7b0a1a2e
qwertyvc4 <135412875+qwertyvc4@users.noreply.github.com>
2023-09-06 09:16:28 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Рязанова Юлия");
+            Console.WriteLine();
+            Console.WriteLine("*******************");
+            Console.WriteLine("Вывод старых и новых цен");
+            Console.WriteLine("*******************");
+            int a, c, b, d, p;
+            Console.WriteLine("Процент:");
+            p = Convert.ToInt32(Console.ReadLine ());
+            Console.WriteLine("Старая цена-1:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Старая цена-2:");
+            c = Convert.ToInt32(Console.ReadLine());
+            b = a + a * p / 100;
+            d = c + c * p / 100;
+            Console.WriteLine("Новая цена-1 = " + b);
+            Console.WriteLine("Новая цена-2 = " + d);
+
+            while (Console.ReadKey().Key!=ConsoleKey.Escape) ;
+
+        }
+
+    }
+}
