636635d2fca21d46e4e8970c754b6ad3d1032887
qwertyvc4 <135412875+qwertyvc4@users.noreply.github.com>
2023-09-06 09:25:44 +0300

    Add files via upload

---
+++ прак 6.1/прак 6.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace прак_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int w = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[w];
+            int i;
+            for (i = 0; i < w; i++)
+            {
+                Console.Write("Введите элемент массива: ");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int r = a[1] - a[0];
+            for (i = 1; i < w; i++)
+            {
+                if (r != a[i] - a[i - 1])
+                {
+                    r = 0;                   
+                }
+            }
+            if (r > 0)
+            {
+                Console.WriteLine("Разность: " + r);
+            }
+            if (r == 0)
+            {
+                Console.WriteLine("Нет ариф. п: " + r);
+            }
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+    }
+}
---
+++ прак 6.2/прак 6.2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace прак_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[x];
+            int i;
+            for (i = 0; i < x; i++)
+            {
+                Console.Write("Введите элемент массива: ");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("По возрастанию - 1 / по убыванию - 2: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            switch (c)
+            {
+                case 1:
+                    Console.WriteLine("По возрастанию: ");
+                    Array.Sort(a);
+                    foreach (int n in a)
+                        Console.WriteLine(n);
+                    break;
+                case 2:
+                    Console.WriteLine("По убыванию: ");
+                    Array.Sort(a);
+                    Array.Reverse(a);
+                    foreach (int n in a)
+                        Console.WriteLine(n);
+                    break;
+            }
+
+            int v = 1;
+            for (i = 1; i < x; i++)
+            {
+                if (a[i - 1] != a[i])                
+                    v++;               
+            }
+            Console.WriteLine("Кол-во различных: " + v);
+            
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+    }
+}
