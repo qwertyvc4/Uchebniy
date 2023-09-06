ff602a772f4fbf47fdbf2c67c98e87c8ad530598
qwertyvc4 <135412875+qwertyvc4@users.noreply.github.com>
2023-09-06 09:22:13 +0300

    Add files via upload

---
+++ прак 5_1/прак 5_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.NetworkInformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+
+namespace Прак_3__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вводим A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Вводим B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            
+            while (A <= B)
+            {
+                int i;
+                for (i = B; i >= A; i--)
+                {
+                    Console.Write(i * i * i + " ");
+                }
+                break;
+            }
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+
+        }
+    }
+}
